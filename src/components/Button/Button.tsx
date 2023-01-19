3183bcb54b83ee06327fe205ac49cef4269646f5
nabepa <nabepadev@gmail.com>
2023-01-19 22:09:07 +0900

    feat: created Button Component
    It is diabled until the MouseEventHandler is over.

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,45 @@
+import {
+  forwardRef,
+  memo,
+  MouseEvent,
+  MouseEventHandler,
+  useState,
+} from 'react';
+
+type OnClick =
+  | MouseEventHandler<HTMLButtonElement>
+  | ((e: MouseEvent<HTMLButtonElement>) => Promise<void>);
+
+type ButtonProps = Omit<React.ComponentPropsWithoutRef<'button'>, 'onClick'> & {
+  onClick?: OnClick | undefined;
+};
+
+const Button: React.FC<ButtonProps> = forwardRef<
+  HTMLButtonElement,
+  ButtonProps
+>(({ children, disabled, onClick, ...attributes }, ref) => {
+  const [isDelayed, setIsDelayed] = useState<boolean>(false);
+  const handleClick: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    if (onClick == null) return;
+    try {
+      setIsDelayed(true);
+      await onClick(e);
+    } finally {
+      setIsDelayed(false);
+    }
+  };
+
+  return (
+    <button
+      disabled={disabled || isDelayed}
+      onClick={handleClick}
+      ref={ref}
+      {...attributes}
+    >
+      {children}
+    </button>
+  );
+});
+
+Button.displayName = 'Button';
+export default memo(Button);

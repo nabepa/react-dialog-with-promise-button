c5eec56dae95e17606e1e1bb1ae95fd0be740285
nabepa <nabepadev@gmail.com>
2023-01-19 23:00:21 +0900

    feat: delayed all buttons during one of buttons is deleayed

--- src/components/ModalDialog/ModalDialog.tsx
+++ src/components/ModalDialog/ModalDialog.tsx
@@ -1,4 +1,12 @@
-import { forwardRef, memo, useImperativeHandle, useRef } from 'react';
+import {
+  forwardRef,
+  memo,
+  useImperativeHandle,
+  useRef,
+  MouseEvent,
+  useCallback,
+  useState,
+} from 'react';
 import Button, { ButtonProps } from '../Button/Button';
 
 export type ModalDialogHandler = {
@@ -49,34 +57,45 @@ const ModalDialog = forwardRef<ModalDialogHandler, Props>(
       [dialogRef]
     );
 
+    const [isDelayed, setIsDelayed] = useState<boolean>(false);
+    const f = useCallback(
+      async (
+        e: MouseEvent<HTMLButtonElement>,
+        key: string,
+        handleClick: ButtonProps['onClick']
+      ) => {
+        try {
+          if (handleClick == null) return;
+          setIsDelayed(true);
+          await handleClick(e);
+        } finally {
+          setIsDelayed(false);
+          e.target.dispatchEvent(new Event(key, { bubbles: true }));
+        }
+      },
+      [isDelayed]
+    );
+
     return (
       <dialog ref={dialogRef}>
         <h1>{title}</h1>
         <p>{content}</p>
-        <Button
-          onClick={async (e) => {
-            try {
-              if (primaryButton.onClick == null) return;
-              await primaryButton.onClick(e);
-            } finally {
-              e.target.dispatchEvent(new Event('primary', { bubbles: true }));
-            }
-          }}
-        >
-          {primaryButton.children}
-        </Button>
-        <Button
-          onClick={async (e) => {
-            try {
-              if (secondaryButton.onClick == null) return;
-              await secondaryButton.onClick(e);
-            } finally {
-              e.target.dispatchEvent(new Event('secondary', { bubbles: true }));
-            }
-          }}
-        >
-          {secondaryButton.children}
-        </Button>
+        <fieldset disabled={isDelayed}>
+          <Button
+            onClick={async (e) => {
+              await f(e, 'primary', primaryButton.onClick);
+            }}
+          >
+            {primaryButton.children}
+          </Button>
+          <Button
+            onClick={async (e) => {
+              await f(e, 'secondary', secondaryButton.onClick);
+            }}
+          >
+            {secondaryButton.children}
+          </Button>
+        </fieldset>
       </dialog>
     );
   }
